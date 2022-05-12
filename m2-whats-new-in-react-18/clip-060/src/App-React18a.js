bc9411d5faecacdd4055b1b9f050bb982b3f6d07
pkellner <peter@peterkellner.net>
2022-05-11 17:34:22 -0700

    added m2 clip

---
+++ m2-whats-new-in-react-18/clip-060/src/App-React18a.js
@@ -0,0 +1,29 @@
+import { Suspense } from "react";
+function App({ displayCount }) {
+  const specialPromiseResource = createSpecialPromise(
+    `/api/cities?displayCount=${displayCount}`
+  );
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RenderComponent resource={specialPromiseResource} />
+    </Suspense>
+  );
+}
+
+function RenderComponent({ specialPromiseResource }) {
+  const cities = specialPromiseResource.cities.read();
+  return (
+    <ul>{cities.map((rec) => (
+        <li key={rec.id}>{rec.name}</li>))}
+    </ul>
+  );
+}
+function createSpecialPromise(url) {
+  const cityPromise = fetchCityPromise(url);
+  return {
+    // wrapPromise does work of returning data
+    //  or throws special return handled internally
+    //  by suspense. (next module for details...)
+    cities: wrapPromise(cityPromise)
+  }
+}
--- m2-whats-new-in-react-18/readme.md
+++
@@ -1,1 +0,0 @@
-# There is no associated source for module 3 of this course.  All the code is pseudo code and in the slides
