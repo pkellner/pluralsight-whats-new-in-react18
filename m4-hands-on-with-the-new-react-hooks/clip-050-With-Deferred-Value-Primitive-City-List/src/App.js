6c16777f0ebccd6e8f29204849ad182ac306f5bc
pkellner <peter@peterkellner.net>
2022-05-16 09:55:08 -0700

    working deferredValue

---
+++ m4-hands-on-with-the-new-react-hooks/clip-050-With-Deferred-Value-Primitive-City-List/src/App.js
@@ -0,0 +1,82 @@
+import "./App.css";
+import { useState, useDeferredValue, memo } from "react";
+import { fetchCityListData } from "./dataApi/fetchCityListData";
+import { Suspense } from "react";
+const displayCount = 1000;
+
+// code similar to example by React Team: https://github.com/reactwg/react-18/discussions/129#discussioncomment-2439125
+
+const ListItem = ({ id, name }) => {
+  let now = performance.now();
+  while (performance.now() - now < 5) {
+    // This loop is intentially here just to drag the component
+    // down in a hard running loop.  It could represent something
+    // like a complex calculation involving drawing a city shape
+    // or something else compute intensive. It's mean to represent
+    // work that can not be easily optimized or removed.
+  }
+  return (
+    <li key={id} className="list-group-item list-group-item-action">
+      {name}-{id}
+    </li>
+  );
+};
+
+const CityListResult = memo(({ cities, searchText }) => (
+  <>
+    {cities
+      .filter(({ name }) => {
+        return (
+          searchText.length === 0 ||
+          name.toLowerCase().includes(searchText.toLowerCase())
+        );
+      })
+      .map((rec) => (
+        <ListItem key={rec.id} id={rec.id} name={rec.name} />
+      ))}
+  </>
+));
+
+function RenderComponent({ resource }) {
+  const cities = resource?.cities.read();
+
+  const [searchText, setSearchText] = useState("");
+  const deferredText = useDeferredValue(searchText);
+
+  const searchValue = deferredText; // this should be searchText or deferredText
+
+  return (
+    <div className="container">
+      <h1>React 18 with Suspense</h1>
+      <div className="col-12">
+        <ul className="list-group city--list">
+          <li className="list-group-item city--header">City List</li>
+          <li className="list-group-item city--header">
+            <input
+              type="search"
+              placeholder="Search"
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }}
+            />
+          </li>
+          <li className="list-group-item city--header">
+            Search Text: {searchValue}
+          </li>
+          <CityListResult cities={cities} searchText={searchValue} />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const resource = fetchCityListData(displayCount);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RenderComponent resource={resource} />
+    </Suspense>
+  );
+}
+
+export default App;
